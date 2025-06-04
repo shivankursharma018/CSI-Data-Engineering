2094dea748e01ebb6ac5ce0795741fae2429f7ab
shivankursharma018 <shivankursharma018@gmail.com>
2025-06-04 23:17:16 +0530

    python arithmetic operators

---
+++ week01/task1.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(f"{a+b}\n{a-b}\n{a*b}")
+    
