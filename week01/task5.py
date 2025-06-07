3d3df1862fdd5ea728fa33e7a5aaaccc4c01ce98
shivankursharma018 <shivankursharma018@gmail.com>
2025-06-07 22:59:58 +0530

    probaility of selected character 'a' in list

---
+++ week01/task5.py
@@ -0,0 +1,23 @@
+# https://www.hackerrank.com/challenges/iterables-and-iterators/problem?isFullScreen=false
+
+from itertools import combinations
+
+N = int(input())
+input_str = input().split()
+print(input_str)
+
+id = int(input())
+select = 'a'
+
+combs = list(combinations(input_str, id))
+print(combs)
+
+fav_combs = 0
+for c in combs:
+    if select in c:
+        fav_combs += 1
+if len(combs) > 0:
+    probability = fav_combs / len(combs)
+    print(f"{probability:.3f}")
+else:
+    print("0")
