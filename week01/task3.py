4c86a58d76c32979e0380275016a471af8f3d978
shivankursharma018 <shivankursharma018@gmail.com>
2025-06-07 22:33:06 +0530

    minion game logic change

--- week01/task3.py
+++ week01/task3.py
@@ -1,31 +1,24 @@
-def minion_game(str):
+def minion_game(string):
     # your code goes here
-    str = str.upper()
+    S = string.upper()
+    n = len(S)
+    kevin_score = 0
+    stuart_score = 0
     vowels = 'AEIOU'
 
-    combinations = []
-    v_score = 0
-    c_score = 0
+    for i in range(n):
+        if S[i] in vowels:
+            kevin_score += (n - i)
+        else:
+            stuart_score += (n - i)
 
-    for i in range(len(str)):
-        for j in range(i+1, len(str) + 1):
-            combinations.append(str[i:j])
-    # print(combinations)
-
-    for v in combinations:
-        if v[0] in vowels:
-            v_score += 1
-
-    for c in combinations:
-        if c[0] not in vowels:
-            c_score += 1
-
-    if v_score > c_score:
-        print(f"Kevin {v_score}")
-    elif v_score == c_score:
-        print("Draw")
+    if kevin_score > stuart_score:
+        print("Kevin", kevin_score)
+    elif stuart_score > kevin_score:
+        print("Stuart", stuart_score)
     else:
-        print(f"Stuart {c_score}")
+        print("Draw")
+
         
 if __name__ == '__main__':
     s = input()
