1b50c91a4ea2469b4940883418ec272c1174367d
shivankursharma018 <shivankursharma018@gmail.com>
2025-06-07 23:38:29 +0530

    string formatting with width and constraints

--- week01/task8.py
+++ week01/task8.py
@@ -1,11 +1,17 @@
 # https://www.hackerrank.com/challenges/python-string-formatting/problem
 
 def print_formated(num):
-    for i in range(1, num+1):
-        decimal = str(i)
-        octal = oct(i)[2:]
-        hexadecimal = hex(i)[2:].upper()
-        binary = bin(i)[2:]
-        print(f"{decimal:>3} {octal:>3} {hexadecimal:>3} {binary:>3}")
+    if num < 1 or num > 99:
+        print("constraint error")
+    else:
+        width = len(bin(num)) - 2
+
+        for i in range(1, num+1):
+            decimal = str(i)
+            octal = oct(i)[2:]
+            hexadecimal = hex(i)[2:].upper()
+            binary = bin(i)[2:]
+            print(f"{decimal:>{width}} {octal:>{width}} {hexadecimal:>{width}} {binary:>{width}}")
+
 num = int(input())
 print_formated(num)
