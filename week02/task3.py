4776b6de6967988d1c840b97a122c5e5277a4c51
shivankursharma018 <shivankursharma018@gmail.com>
2025-06-11 23:00:00 +0530

    textwrap module

---
+++ week02/task3.py
@@ -0,0 +1,6 @@
+import textwrap
+def wrap(string, max_width):
+    return '\n'.join(textwrap.wrap(string, max_width))
+string, max_width = input(), int(input())
+result = wrap(string, max_width)
+print(result)
