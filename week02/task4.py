e47bbcf944249a2d7d2330d0f48b35df141be51f
shivankursharma018 <shivankursharma018@gmail.com>
2025-06-13 17:06:10 +0530

    pattern logic and alphabet logic isolated

--- week02/task4.py
+++ week02/task4.py
@@ -1,25 +1,27 @@
 def alphabet_rangoli(size):
-    for i in range(0, size+1):
-        # print('-', end='')
-
-        for j in range(0, i):
-            print('#', end='-')
-    
-        print(('-'*(size-i-1)), end='')
-        print()
-        print(('-'*(size-i-1)), end='')
-
-
-    print('-', end='')
-    for m in range(size-1, 0, -1):
-        # print('-', end='')
-
-        for n in range(0, m):
-            print('#', end='-')
-
-        print(('-'*(size-m-1)), end='')
-        print()
-        print(('-'*(size-m+1)), end='')
+    s = 'A'
+    size = 5
+    size += 2
+    j = size-1
+    k = size-1
+    for i in range(1,size,2):
+        print('-'*j+'-'.join(s*i)+'-'*k)
+        j = j-2
+        k = k-2
+    j = 0
+    k = 0
+    for m in range(size,0,-2):
+        print('-'*j+'-'.join(s*m)+'-'*k)
+        j = j+2
+        k = k+2
 
 size = int(input())
 alphabet_rangoli(size)
+# 3
+# 3 2 1 2 3
+for i in range(size, 0, -1):
+    initial_char = chr(97+i-1)
+    print(initial_char, end='')
+for j in range(2, size+1):
+    later_char = chr(97+j-1)
+    print(later_char, end='')
