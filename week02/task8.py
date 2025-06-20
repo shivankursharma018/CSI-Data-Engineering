735db43ac8a25adee488d670a3460f3400b21b23
shivankursharma018 <shivankursharma018@gmail.com>
2025-06-20 23:42:04 +0530

    regex problem updated

--- week02/task8.py
+++ week02/task8.py
@@ -1,9 +1,12 @@
 # https://www.hackerrank.com/challenges/incorrect-regex/problem
 import re
-t = int(input())
-for i in range(t):
-    regex_in = input()
-    if re.compile(regex_in):
-        print(True)
-    else:
-        print(False)
+def solve_for_regex():
+    num_test_cases = int(raw_input()) 
+    for i in range(num_test_cases):
+        regex_string = raw_input()
+        try:
+            re.compile(regex_string)
+            print "True" 
+        except re.error:
+            print "False"
+solve_for_regex()
