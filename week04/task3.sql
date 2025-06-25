b0f5383f3dd3444a4ed38f14eac121f84b1f6857
shivankursharma018 <shivankursharma018@gmail.com>
2025-06-25 23:55:29 +0530

    adding hackerrank problem link

--- week04/task3.sql
+++ week04/task3.sql
@@ -1,1 +1,2 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-19/problem
 SELECT ROUND(SQRT(POWER(max(lat_n)-min(lat_n), 2) + POWER(max(long_w)-min(long_w), 2)), 4) FROM station;
