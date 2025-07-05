5ed4f88178d5f9b19150f87e7f5dee7187d320a0
shivankursharma018 <shivankursharma018@gmail.com>
2025-07-05 18:53:37 +0530

    task 2: event based trigger

--- week05/query.sql
+++ week05/query.sql
@@ -1,3 +1,5 @@
+-- task 1: export sql to csv, avro and parquet
+-- creating sql db and table
 DROP TABLE IF EXISTS Students;
 
 CREATE TABLE Students (
@@ -20,3 +22,17 @@ VALUES
 (8, 'Pooja', 'Aggarwal', 22, 'pooja.aggarwal96@rediffmail.com'),
 (9, 'Yash', 'Kapoor', 21, 'yash.kapoor@zoho.com'),
 (10, 'Simran', 'Gupta', 20, 'simran.gupta24@gmail.com');
+
+-- task 2: schedule based and event based trigger
+-- schedule based trigger
+INSERT INTO Students (StudentID, FirstName, LastName, Age, Email)
+VALUES 
+(11, 'Ananya', 'Sharma', 22, 'ananya.sharma@edu.in'),
+(12, 'Karan', 'Mehta', 23, 'karan.mehta1999@gmail.com'),
+(13, 'Divya', 'Iyer', 21, 'divya.iyer@outlook.com'),
+(14, 'Rajat', 'Bansal', 20, 'rajat.bansal@icloud.com'),
+(15, 'Sneha', 'Patel', 24, 'sneha.patel@protonmail.com');
+
+-- event based trigger (file arrival)
+-- remove last 5 lines from input/csv/data2.csv
+-- run event trigger, new changes made to input/parquet/data2.parquet
